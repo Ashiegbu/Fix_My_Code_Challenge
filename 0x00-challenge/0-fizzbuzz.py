138652423559102bd42f62ce2aed463f7eefbae4
Ashiegbu <47593584+Ashiegbu@users.noreply.github.com>
2023-02-13 09:55:15 +0100

    FizzBuzz
    FizzBuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
-        sys.exit(1)
+        sys.exit(1
 
     number = int(sys.argv[1])
     fizzbuzz(number)
